a4ada6176183f432ebb6917ca4785c3f49b3a985
Leo Souza <leonardodomaria@gmail.com>
2023-12-10 15:19:18 -0300

    Aula 1

---
+++ go/internal/market/entity/investor.go
@@ -0,0 +1,48 @@
+package entity
+
+type Investor struct {
+	ID            string
+	Name          string
+	AssetPosition []*InvestorAssetPosition
+}
+
+func NewInvestor(id string) *Investor {
+	return &Investor{
+		ID:            id,
+		AssetPosition: []*InvestorAssetPosition{},
+	}
+}
+
+func (i *Investor) AddAssetPossition(assetPosition *InvestorAssetPosition) {
+	i.AssetPosition = append(i.AssetPosition, assetPosition)
+}
+
+func (i *Investor) UpadteAssetPosition(assetID string, qtdShares int) {
+	assetPosition := i.GetAssetPosition(assetID)
+	if assetPosition == nil {
+		i.AssetPosition = append(i.AssetPosition, NewInvestorAssetPosition(assetID, qtdShares))
+	} else {
+		assetPosition.Shares += qtdShares
+	}
+}
+
+func (i *Investor) GetAssetPosition(assetID string) *InvestorAssetPosition {
+	for _, assetPostion := range i.AssetPosition {
+		if assetPostion.AssetID == assetID {
+			return assetPostion
+		}
+	}
+	return nil
+}
+
+type InvestorAssetPosition struct {
+	AssetID string
+	Shares  int
+}
+
+func NewInvestorAssetPosition(assetID string, shares int) *InvestorAssetPosition {
+	return &InvestorAssetPosition{
+		AssetID: assetID,
+		Shares:  shares,
+	}
+}
